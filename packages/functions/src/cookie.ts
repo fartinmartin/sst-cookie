0075e5b1e54b24700f52762e32467bce2993e749
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2024-02-06 21:46:34 -0800

    simple cookies

--- packages/functions/src/cookie.ts
+++ packages/functions/src/cookie.ts
@@ -1,7 +1,8 @@
 import { ApiHandler } from "sst/node/api";
+import { parse } from "cookie";
 
 export const main = ApiHandler(async ({ cookies }) => {
-	const sentToken = parse(cookies);
+	const cooks = JSON.stringify(parse(cookies?.join(";") ?? ""), null, 2);
 	const token = "HELLO";
 
 	const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
@@ -9,23 +10,9 @@ export const main = ApiHandler(async ({ cookies }) => {
 
 	return {
 		statusCode: 200,
-		body: `Hello world. The time is ${new Date().toISOString()}. ${
-			sentToken
-				? `\n\nYour request came with an "test-token" cookie with the value: ${sentToken}`
-				: ""
-		}`,
+		body: `Hello world. The time is ${new Date().toISOString()}.\n\nYour request came with the following cookies: ${cooks}`,
 		cookies: [
 			`test-token=${token}; HttpOnly; SameSite=None; Secure; Path=/; Expires=${expires}`,
 		],
 	};
 });
-
-function parse(cookies: string[] | undefined) {
-	if (cookies?.length) {
-		const cookie = cookies[0];
-		const token = cookie.split("=")[1];
-		return token;
-	} else {
-		return undefined;
-	}
-}
